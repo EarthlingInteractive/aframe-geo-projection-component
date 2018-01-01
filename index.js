b292029c64b3901e816894c0a0e49292290d970b
Steve Brudz <sbrudz@gmail.com>
2018-01-01 11:35:45 -0600

    Add d3 projection support to component

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 /* global AFRAME */
 
+var d3 = Object.assign({}, require('d3-scale'), require('d3-geo'), require('d3-geo-projection'));
+
 if (typeof AFRAME === 'undefined') {
   throw new Error('Component attempted to register before AFRAME was available.');
 }
@@ -11,6 +13,9 @@ AFRAME.registerComponent('geo-projection', {
   schema: {
     src: {
       type: 'asset'
+    },
+    projection: {
+      default: 'geoIdentity'
     },
     width: {default: 1},
     height: {default: 1}
@@ -31,5 +36,57 @@ AFRAME.registerComponent('geo-projection', {
    * Called when a component is removed (e.g., via removeAttribute).
    * Generally undoes all modifications to the entity.
    */
-  remove: function () { }
+  remove: function () { },
+
+  /**
+   * @returns the d3 projection or transform specified by the projection attribute
+   */
+  getD3Projection: function () {
+    var projection = d3[this.data.projection];
+    if (typeof projection !== 'function') {
+      throw Error('Invalid d3 projection; use a projection from d3-geo or d3-geo-projection');
+    }
+    return projection();
+  },
+
+  /**
+   * @returns a d3 transform that converts from normal SVG screen coordinates
+   *          (an origin of [0,0] with y pointing down) to A-Frame coordinates
+   *          where the extent is based on the height and width, the origin is
+   *          in the center, and y points up
+   */
+  getWorldTransform: function () {
+    const height = this.data.height;
+    const width = this.data.width;
+
+    const x = d3.scaleLinear().domain([0, width]).range([-width / 2, width / 2]);
+    const y = d3.scaleLinear().domain([0, height]).range([height / 2, -height / 2]);
+
+    return d3.geoTransform({
+      point: function (px, py) {
+        this.stream.point(x(px), y(py));
+      }
+    });
+  },
+
+  /**
+   * @param geoJson the geometry to use for scaling and centering
+   * @returns a d3 projection stream which centers the given geoJson in
+   *          A-Frame coordinates and scales it to fit the height and width
+   *          of the component
+   */
+  getFittedProjection: function (geoJson) {
+    const height = this.data.height;
+    const width = this.data.width;
+
+    var projection = this.getD3Projection().fitSize([width, height], geoJson);
+    var worldTransform = this.getWorldTransform();
+    // Thanks to this StackOverflow answer on how to chain streams:
+    // https://stackoverflow.com/a/31647135
+    return {
+      stream: function (s) {
+        return projection.stream(worldTransform.stream(s));
+      }
+    };
+  }
 });
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,5 +1,6 @@
 /* global assert, setup, suite, test */
 require('aframe');
+var d3 = require('d3-geo');
 require('../index.js');
 var entityFactory = require('./helpers').entityFactory;
 
@@ -43,6 +44,72 @@ suite('geo-projection component', function () {
       test('defaults to empty string', function () {
         assert.propertyVal(component.data, 'src', '');
       });
+    });
+    suite('projection property', function () {
+      test('exists', function () {
+        assert.property(component.data, 'projection');
+      });
+      test('defaults to geoIdentity', function () {
+        assert.propertyVal(component.data, 'projection', 'geoIdentity');
+      });
+    });
+  });
+
+  suite('#getD3Projection', function () {
+    test('exists as a method on the component', function () {
+      assert.property(component, 'getD3Projection');
+    });
+    test('uses the projection attribute to look up a d3 projection', function () {
+      el.setAttribute('geo-projection', {
+        projection: 'geoAlbers'
+      });
+      assert.equal(component.getD3Projection().toString(), d3.geoAlbers().toString());
+    });
+    test('can also be a d3 transform', function () {
+      assert.equal(component.getD3Projection().toString(), d3.geoIdentity().toString());
+    });
+    test('throws an error if an invalid projection is specified', function () {
+      el.setAttribute('geo-projection', {
+        projection: 'badStuff'
+      });
+      assert.throws(component.getD3Projection.bind(component), 'Invalid d3 projection; use a projection from d3-geo or d3-geo-projection');
+    });
+  });
+
+  suite('#getWorldTransform', function () {
+    test('exists as a method on the component', function () {
+      assert.property(component, 'getWorldTransform');
+    });
+    test('returns a transform that maps from d3 space to A-Frame space', function () {
+      var width = 20;
+      var height = 10;
+      el.setAttribute('geo-projection', {
+        width: width,
+        height: height
+      });
+      var geoJson = {type: 'LineString', coordinates: [[0, 0], [width, height]]};
+      var worldTransform = component.getWorldTransform();
+      var path = d3.geoPath().projection(worldTransform);
+      var result = path(geoJson);
+      assert.equal(result, 'M-10,5L10,-5', 'Line coords should start at top left corner and end at bottom right corner');
+    });
+  });
+
+  suite('#getFittedProjection', function () {
+    test('exists as a method on the component', function () {
+      assert.property(component, 'getFittedProjection');
+    });
+    test('returns a projection fits and centers the given geoJson in A-Frame space', function () {
+      var geoJson = {type: 'LineString', coordinates: [[0, 0], [100, 100]]};
+      el.setAttribute('geo-projection', {
+        projection: 'geoIdentity',
+        width: 2,
+        height: 2
+      });
+      var projection = component.getFittedProjection(geoJson);
+      var path = d3.geoPath().projection(projection);
+      var result = path(geoJson);
+      assert.equal(result, 'M-1,1L1,-1', 'Line coords should start at top left corner and end at bottom right corner');
     });
   });
 });
