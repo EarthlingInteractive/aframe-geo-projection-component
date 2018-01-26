2b36e351bf7968fa5a647d0beb07aef0789e48e5
Steve Brudz <sbrudz@gmail.com>
2018-01-26 16:44:51 -0600

    Add implementation and tests for ThreeJSRenderContext

--- src/renderer.js
+++ src/renderer.js
@@ -1,7 +1,54 @@
 
-function ThreeJSRenderContext() {
-
+function ThreeJSRenderContext(shapePath) {
+  this.shapePath = shapePath;
 }
+
+ThreeJSRenderContext.prototype.beginPath = function beginPath () {
+ // no-op
+};
+
+ThreeJSRenderContext.prototype.moveTo = function moveTo (x, y) {
+  this.shapePath.moveTo(x, y);
+};
+
+ThreeJSRenderContext.prototype.lineTo = function lineTo (x, y) {
+  this.shapePath.lineTo(x, y);
+};
+
+ThreeJSRenderContext.prototype.arc = function arc (x, y, radius, startAngle, endAngle) {
+  this.shapePath.currentPath.arc(x, y, radius, startAngle, endAngle);
+};
+
+ThreeJSRenderContext.prototype.closePath = function closePath () {
+  this.shapePath.currentPath.closePath();
+};
+
+ThreeJSRenderContext.prototype.toShapes = function toShapes (isCCW) {
+  this.shapePath.toShapes(isCCW);
+};
+
+ThreeJSRenderContext.prototype.toVertices = function toVertices () {
+  const verticesForShape = [];
+  this.shapePath.subPaths.forEach(function (path) {
+    path.curves.forEach(function (curve) {
+      if (curve.isLineCurve) {
+        verticesForShape.push(curve.v1.x);
+        verticesForShape.push(curve.v1.y);
+        verticesForShape.push(0);
+        verticesForShape.push(curve.v2.x);
+        verticesForShape.push(curve.v2.y);
+        verticesForShape.push(0);
+      } else {
+        curve.getPoints().forEach(function (point) {
+          verticesForShape.push(point.x);
+          verticesForShape.push(point.y);
+          verticesForShape.push(0);
+        });
+      }
+    });
+  });
+  return verticesForShape;
+};
 
 module.exports = {
   /**
--- tests/src/renderer.test.js
+++ tests/src/renderer.test.js
@@ -52,6 +52,77 @@ suite('renderer', function () {
     });
   });
   suite('ThreeJSRenderContext', function () {
+    test('implements the d3-geo required subset of the CanvasRenderingContext2D API', function () {
+      var context = new renderer.ThreeJSRenderContext();
+      assert.property(context, 'beginPath');
+      assert.property(context, 'moveTo');
+      assert.property(context, 'lineTo');
+      assert.property(context, 'arc');
+      assert.property(context, 'closePath');
+    });
+    suite('delegates to the given THREE.ShapePath functions', function () {
+      test('beginPath is a no-op', function () {
+        var mockShapePath = { beginPath: sinon.spy() };
+        var context = new renderer.ThreeJSRenderContext(mockShapePath);
+        context.beginPath();
+        sinon.assert.notCalled(mockShapePath.beginPath);
+      });
 
+      test('moveTo', function () {
+        var mockShapePath = { moveTo: sinon.spy() };
+        var context = new renderer.ThreeJSRenderContext(mockShapePath);
+        context.moveTo(1, 2);
+        sinon.assert.calledWith(mockShapePath.moveTo, 1, 2);
+      });
+
+      test('lineTo', function () {
+        var mockShapePath = { lineTo: sinon.spy() };
+        var context = new renderer.ThreeJSRenderContext(mockShapePath);
+        context.lineTo(1, 2);
+        sinon.assert.calledWith(mockShapePath.lineTo, 1, 2);
+      });
+
+      test('arc', function () {
+        var mockShapePath = { currentPath: { arc: sinon.spy() } };
+        var context = new renderer.ThreeJSRenderContext(mockShapePath);
+        context.arc(1, 2, 3, 4, 5);
+        sinon.assert.calledWith(mockShapePath.currentPath.arc, 1, 2, 3, 4, 5);
+      });
+
+      test('closePath', function () {
+        var mockShapePath = { currentPath: { closePath: sinon.spy() } };
+        var context = new renderer.ThreeJSRenderContext(mockShapePath);
+        context.closePath();
+        sinon.assert.calledOnce(mockShapePath.currentPath.closePath);
+      });
+    });
+    suite('provides functions to extract path data', function () {
+      test('#toShapes', function () {
+        var mockShapePath = { toShapes: sinon.spy() };
+        var context = new renderer.ThreeJSRenderContext(mockShapePath);
+        context.toShapes(true);
+        sinon.assert.calledWith(mockShapePath.toShapes, true);
+      });
+      test('#toVertices', function () {
+        var mockShapePath = { subPaths: [
+            { curves: [
+                {
+                  isLineCurve: true,
+                  v1: { x: 1, y: 2 },
+                  v2: { x: 3, y: 4 }
+                },
+                {
+                  isLineCurve: false,
+                  getPoints: function() {
+                    return [ { x: 5, y: 6 }]
+                  }
+                }
+              ] }
+          ] };
+        var context = new renderer.ThreeJSRenderContext(mockShapePath);
+        var vertices = context.toVertices();
+        assert.deepEqual(vertices, [1, 2, 0, 3, 4, 0, 5, 6, 0]);
+      });
+    });
   });
 });
