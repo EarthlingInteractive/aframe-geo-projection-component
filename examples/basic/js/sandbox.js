3a85a443326fb592f2ca54acac2bbc26d4802554
Don Smith <don.c.smith@gmail.com>
2018-03-15 16:20:16 -0500

    textures added, skybox added, respawn fallen shapes, kick only on the ground

--- examples/basic/js/sandbox.js
+++ examples/basic/js/sandbox.js
@@ -1,44 +1,55 @@
 var sceneBounds = { x: 10, y: 10, z: 10 };
-var attributes = ['geometry', 'position', 'color', 'dynamic-body', 'static-body']
+var attributes = ['geometry', 'position', 'color', 'dynamic-body', 'static-body', 'src'];
+var tesselatedTextures = [
+	// 'grass.jpg',
+	'grey-cement-pavers.jpg',
+	'mars-rock.jpg',
+	'water-01.jpg',
+	'brick-01.jpg',
+	'ivy.jpg',
+	'hedge.jpg',
+	'wood-planks-01.jpg',
+	'wood-parquet-01.jpg'
+];
 var positionRange = {
 	x: [-40, 40],
-	y: [10, 100],
+	y: [100, 1000],
 	z: [-40, 40]
 };
 var shapeGeoms = {
 	sphere: {
-		radius: [0.25, 3]
+		radius: [0.25, 8]
 	},
 	box: {
-		width: [0.25, 4],
-		depth: [0.25, 4],
-		height: [0.25, 4]
+		width: [0.25, 6],
+		depth: [0.25, 6],
+		height: [0.25, 6]
 	},
 	cylinder: {
 		radius: [0.25, 4],
-		height: [1, 4]
+		height: [1, 8]
 	},
 	cone: {
-		topRadius: [0.25, 4],
-		bottomRadius: [0.25, 4],
-		height: [1, 4]
+		radiusTop: [0.25, 6],
+		radiusButtom: [0.25, 6],
+		height: [1, 8]
 	},
 	dodecahedron: {
-		radius: [0.25, 5]
+		radius: [0.25, 8]
 	},
 	octahedron: {
-		radius: [0.25, 5]
+		radius: [0.25, 8]
 	},
 	tetrahedron: {
-		radius: [0.25, 5]
+		radius: [0.25, 8]
 	},
 	torus: {
-		radius: [1, 5],
-		radiusTubular: [0.25, 2]
+		radius: [2, 8],
+		radiusTubular: [0.25, 3]
 	},
 	torusKnot: {
-		radius: [1, 5],
-		radiusTubular: [0.25, 2],
+		radius: [2, 8],
+		radiusTubular: [0.25, 3],
 		p: Math.floor((Math.random() * 4) + 2),
 		q: Math.floor((Math.random() * 4) + 2)
 	}
@@ -56,9 +67,13 @@ var Shape = function(options) {
 		this.type = shapeTypes[Math.floor(Math.random() * shapeTypes.length)];
 	}
 
-	if (typeof this.color === 'undefined') {
-		// https://www.paulirish.com/2009/random-hex-color-code-snippets/
-		this.color = '#'+Math.floor(Math.random()*16777215).toString(16);
+	if (typeof this.color === 'undefined' && typeof this.src === 'undefined') {
+		if (Math.random() < 0.1) {
+			this.color = '#'+Math.floor(Math.random()*16777215).toString(16);
+		} else {
+			var src = tesselatedTextures[Math.floor(Math.random() * tesselatedTextures.length)];
+			this.src = `assets/tessellated/${src}`;
+		}
 	}
 
 	if (typeof this.position === 'undefined') {
@@ -66,11 +81,6 @@ var Shape = function(options) {
 		Object.keys(positionRange).forEach(k => {
 			this.position[k] = (Math.random() * positionRange[k][1] - positionRange[k][0]) + positionRange[k][0]
 		});
-		// this.position = {
-		// 	x: (Math.random() * sceneBounds.x * 2) - sceneBounds.x,
-		// 	y: (Math.random() * sceneBounds.y * 2) - sceneBounds.y,
-		// 	z: (Math.random() * sceneBounds.z * 2) - sceneBounds.z,
-		// };
 	}
 
 	if (typeof this.geometry === 'undefined') {
@@ -88,6 +98,18 @@ var Shape = function(options) {
 		};
 	}
 };
+
+var CreateAFRameEntity = function(t, $scene) {
+	var $shape = document.createElement(`a-${t.type}`);
+	Object.keys(t)
+		.filter(k => attributes.includes(k))
+		.forEach(k => {
+			$shape.setAttribute(k, t[k])
+		});
+	
+	$scene.appendChild($shape);
+	return $shape;
+}
 
 var things = [
 	{
@@ -111,32 +133,35 @@ var things = [
 var $scene = document.querySelector('a-scene');
 
 var cnt = 0;
-while (cnt++ < 50) {
+while (cnt++ < 100) {
 	var t = new Shape();
 	(function() {
-		var $shape = document.createElement(`a-${t.type}`);
+		var $shape = CreateAFRameEntity(t, $scene);
 		var shapeInterval = ((Math.random() * 2) + 1) * 1000;
-		var shapeImpulse = (Math.random() * 4) + 10
-		Object.keys(t)
-			.filter(k => attributes.includes(k))
-			.forEach(k => {
-				$shape.setAttribute(k, t[k])
-			})
+		var shapeImpulse = (Math.random() * 10) + 40
 		var doBump = () => {
 			if (typeof $shape.body !== 'undefined') {
-				try {
-					$shape.body.applyImpulse(
-						new CANNON.Vec3(0, shapeImpulse, 0)
-					);
-				} catch (ex) {
-					//console.log(ex);
+				var y = $shape.body.position.y;
+				if (y < 5 && y > -5) {
+					try {
+						//console.log($shape.body);
+						
+						$shape.body.applyImpulse(
+							new CANNON.Vec3(0, shapeImpulse, 0)
+						);
+					} catch (ex) {
+						//console.log(ex);
+					}
+				} else if (y < -5) {
+					t = new Shape();
+					$scene.removeChild($shape)
+					$shape = CreateAFRameEntity(t, $scene); //document.createElement(`a-${t.type}`);
 				}
 			}
 			setTimeout(doBump, shapeInterval);
 		};
 		
-		console.log(t, $shape);
-		$scene.appendChild($shape);
+		//console.log(t, $shape);
 		setTimeout(doBump, shapeInterval);
 	})();
 }
