a42518ae3e937f54a6ef2b35b1ce1292fb384627
Don Smith <don.c.smith@gmail.com>
2018-03-21 16:23:27 -0500

    move away from buggy ID based targetting, just pass object

--- examples/basic/js/component/chest.js
+++ examples/basic/js/component/chest.js
@@ -13,18 +13,8 @@ AFRAME.registerComponent('chest', {
 
 	update: function () {
 		var object3D = this.el.object3D;
-
 		var data = this.data;
-		console.log(data);
-
-
-		var ts = (new Date()).getTime()
-
-		console.log(this.el.components);
-
 		this.el.setAttribute('position', this.el.components.position.data);
-		
-		//this.el.setAttribute('dynamic-body', true);
 
 		var boxBottom = document.createElement('a-box');
 		boxBottom.setAttribute('position', this.el.components.position.data);
@@ -33,7 +23,6 @@ AFRAME.registerComponent('chest', {
 		boxBottom.setAttribute('depth', data.dimension);
 		boxBottom.setAttribute('src', 'assets/tessellated/wood-planks-01.jpg');
 		boxBottom.setAttribute('material', { shader: 'flat' });
-		boxBottom.setAttribute('id', `boxBottom${ts}`);
 		boxBottom.setAttribute('static-body', true);
 		boxBottom.setAttribute('physics-body', { mass: 100 });
 
@@ -43,37 +32,16 @@ AFRAME.registerComponent('chest', {
 		boxLid.setAttribute('width', data.dimension);
 		boxLid.setAttribute('height', data.lidThickness);
 		boxLid.setAttribute('depth', data.dimension);
-		boxLid.setAttribute('id', `boxLid${ts}`);
 		boxLid.setAttribute('dynamic-body', true);
 		boxBottom.setAttribute('physics-body', { mass: 1 });
 		boxLid.setAttribute('constraint', { 
 			type: 'hinge',
-			target: `#boxBottom${ts}`,
+			target: boxBottom,
 			axis: { x: 0, y: 0, z: data.dimension },
 			targetAxis: { x: 0, y: 0, z: data.dimension },
-			//pivot: { x: -data.dimension / 2, y: Math.max(0.05, -data.dimension * 0.01) + data.lidThickness, z: 0 },
 			pivot: { x: -data.dimension / 2, y: -0.05, z: 0 },
 			targetPivot: { x: -data.dimension / 2, y: data.dimension / 2, z: 0 }
-			// axis: { x: 0, y: 0, z: 1 },
-			// targetAxis: { x: 0, y: 0, z: 1 },
-			// pivot: { x: -0.5, y: -0.05, z: 0 },
-			// targetPivot: { x: -0.5, y: 0.5, z: 0 }
 		});
-		
-		// <a-entity id="box1" position="0 1 -2">
-		// 	<a-text value="Hinge" position="0 1.5 0" align="center"></a-text>
-		// 	<a-box id="hinge-target" position="0 0.5 0" color="#777" static-body></a-box>
-		// 	<a-box id="lid" height="0.1"
-		// 			color="#F00"
-		// 			dynamic-body
-		// 			constraint="type: hinge;
-		// 						target: #hinge-target;
-		// 						axis: 0 0 1;
-		// 						targetAxis: 0 0 1;
-		// 						pivot: -0.5 -0.05 0;
-		// 						targetPivot: -0.5 0.5 0;">
-		// 	</a-box>
-		// </a-entity>
 
 		this.boxBottom = boxBottom;
 		this.boxLid = boxLid;
