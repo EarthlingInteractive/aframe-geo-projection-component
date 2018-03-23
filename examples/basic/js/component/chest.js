51324f3506762a854d9d66936f7e153b59103ac7
Don Smith <don.c.smith@gmail.com>
2018-03-23 14:51:37 -0500

    better

--- examples/basic/js/component/chest.js
+++ examples/basic/js/component/chest.js
@@ -24,20 +24,22 @@ AFRAME.registerComponent('chest', {
 		boxBack.setAttribute('width', data.dimension);
 		boxBack.setAttribute('height', data.dimension);
 		boxBack.setAttribute('depth', data.dimension);
-		boxBack.setAttribute('material', { shader: 'flat', 'src': 'assets/tessellated/wood-planks-01.jpg' });
+		boxBack.setAttribute('material', { shader: 'flat', 'src': '#wood-planks-01' });
 		boxBack.setAttribute('dynamic-body', true);
-		boxBack.setAttribute('physics-body', { mass: 10000 });
+		//boxBack.setAttribute('static-body', true);
+		boxBack.setAttribute('physics-body', { mass: 1000000000 });
 		
 		var boxLid = document.createElement('a-box');
 		var lidPos = Object.assign({}, pos);
-		lidPos.y += data.lidThickness + 0.01;
-		boxLid.setAttribute('material', { shader: 'flat', src: 'assets/tessellated/wood-planks-02.jpg', rotation: { x: 0, y: 90, z: 0 } });
+		lidPos.y += data.lidThickness + 1.5; //0.01;
+		boxLid.setAttribute('material', { shader: 'flat', src: '#wood-planks-02' });
 		boxLid.setAttribute('width', data.dimension);
 		boxLid.setAttribute('height', data.lidThickness);
 		boxLid.setAttribute('depth', data.dimension);
 		boxLid.setAttribute('dynamic-body', true);
+		//boxLid.setAttribute('static-body', true);
 		boxLid.setAttribute('position', lidPos);
-		boxLid.setAttribute('physics-body', { mass: 1 });
+		boxLid.setAttribute('physics-body', { mass: 0 });
 		boxLid.setAttribute('constraint', { 
 			type: 'hinge',
 			target: boxBack,
@@ -48,11 +50,9 @@ AFRAME.registerComponent('chest', {
 			targetPivot: { x: -data.dimension / 2, y: data.dimension / 2, z: 0 }
 		});
 
-		//this.boxBottom = boxBottom;
 		this.boxBack = boxBack;
 		this.boxLid = boxLid;
 
-		//this.el.appendChild(this.boxBottom);
 		this.el.appendChild(this.boxBack);
 		this.el.appendChild(this.boxLid);
 	},
