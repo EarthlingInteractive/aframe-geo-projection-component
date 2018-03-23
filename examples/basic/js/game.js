df5c5a6def006c9bee9688eeff3eeb1f614b252f
Don Smith <don.c.smith@gmail.com>
2018-03-23 15:18:47 -0500

    open and close the lid

--- examples/basic/js/game.js
+++ examples/basic/js/game.js
@@ -2,5 +2,9 @@ setTimeout(function() {
 	var $chests = document.querySelectorAll('[chest]');
 	var $chestToUse = $chests[Math.floor(Math.random() * $chests.length)];
 	var $lid = $chestToUse.querySelector('.box-lid')
-	$lid.body.applyImpulse(new CANNON.Vec3(0, 40, 0));
+	$lid.body.applyImpulse(new CANNON.Vec3(0, 40, 0), new CANNON.Vec3(0, 0, 0));
+
+	setTimeout(function() {
+		$lid.body.applyImpulse(new CANNON.Vec3(-40, 0, 0), new CANNON.Vec3(0, 0, 0));
+	}, 1500	)
 }, 500)
